ee6064796dc7dae637fd99fea59c4a75942dc612
vgerak <vgerak@gmail.com>
2016-08-20 11:53:19 +0100

    Final touches

--- Snippets/ajax.js
+++ Snippets/ajax.js
@@ -2,7 +2,7 @@ var crackDict = 'static/js/dicts/rockyou.json';
 var guesses;
 // [...]
 $.getJSON(crackDict, function (data) {
-guesses = data;
-if ($("#ppassword").val() !== '')
-  $("#ppassword").change();
+    guesses = data;
+    if ($("#ppassword").val() !== '')
+        $("#ppassword").change();
 });
--- Snippets/get_penalty.js
+++ Snippets/get_penalty.js
@@ -1,16 +1,16 @@
 function letterCount(pw){
     var s = pw.match(/(.)\1{2,}/g) || [];
     return s.map(function(itm){
-      return [itm.charAt(0), itm.length];
+        return [itm.charAt(0), itm.length];
     });
 }
 
 function getScoreAfterPenalties(ppassStr, pw) {
-  // Penalties
-  // Consecutive letters
-  var consecutives = letterCount(pw);
-  for (var i = consecutives.length - 1; i >= 0; i--) {
-    ppassStr += consecutives[i][1] * options.consec_penalty;
-  }
-  return ppassStr;
+    // Penalties
+    // Consecutive letters
+    var consecutives = letterCount(pw);
+    for (var i = consecutives.length - 1; i >= 0; i--) {
+        ppassStr += consecutives[i][1] * options.consec_penalty;
+    }
+    return ppassStr;
 }
